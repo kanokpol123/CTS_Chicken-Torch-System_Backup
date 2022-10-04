f57b03fa5d622f29c325fdb2b3a48c426294b47b
kanokpol123 <kanokpolza32@gmail.com>
2022-10-04 09:26:27 +0700

    ..

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 #include<iostream>
 using namespace std;
+
+//void login(){}
+//void register(){}
+//void mainMenu(){}
+//void showData(){}
+//void showMenu(){}
+//void showQuese(){}
+
+
 int main()
 {
     list <int> mylist;
