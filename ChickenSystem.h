b2050a57ec7e78f815c2c26b83beecbe2d9a8336
Minero20 <pisit.namsiri@gmail.com>
2022-10-08 17:19:26 +0700

    D link list คร่าวๆ

---
+++ ChickenSystem.h
@@ -0,0 +1,16 @@
+#ifndef CHICKENSYSTEM_H
+#define CHICKENSYSTEM_H
+
+#include "Customer.h"
+#include <iterator>
+#include <list>
+#include <queue>
+using namespace std;
+
+class ChickenSystem
+{
+    private:
+        list <Customer*> Customer;
+};
+
+#endif
